387038e56c19afdf125b6a399dfa3b89b5dc5ae3
Marlon <hassan_84@hotmail.es>
2026-02-24 18:25:23 -0500

    por creacion de nueva db

--- setup.sh
+++ setup.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-echo "--- INTENTO DEFINITIVO: LIMPIEZA QUIRÚRGICA ---"
+echo "--- DESPLIEGUE EN BASE DE DATOS LIMPIA ---"
 
-# 1. Limpiamos datos viejos
-python manage.py flush --noinput
-
-# 2. Creamos las tablas (aquí es donde se pueden crear perfiles vacíos)
+# 1. Crear las tablas desde cero
 python manage.py migrate --noinput
 
-# 3. BORRAMOS LOS PERFILES AUTOMÁTICOS (Este es el secreto)
-# Esto limpia la tabla de perfiles para que el JSON no choque
-python manage.py shell -c "from perfiles.models import Perfil; Perfil.objects.all().delete(); print('Tablas de perfiles despejadas')"
-
-# 4. Ahora cargamos tus datos
+# 2. CARGAR TUS DATOS (Ahora no habrá choques)
 python manage.py loaddata datadump.json
 
-# 5. Finalizamos
+# 3. Archivos estáticos y encendido
 python manage.py collectstatic --noinput
 gunicorn MONSTER_APP.wsgi --bind 0.0.0.0:$PORT
